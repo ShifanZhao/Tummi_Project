e00144364b778a4306016c2771371506b086bb09
tiffanyngu12 <tiffanyngu12@gmail.com>
2025-08-11 18:12:23 -0400

    Updated Home Page to add Spencer, the Casual Diner, and the associated page 00_Casual_Diner_Home. Updated Logo.

--- app/src/Home.py
+++ app/src/Home.py
@@ -44,7 +44,7 @@ st.write('#### Hello! As which user would you like to log in?')
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as Casual Diner, the Casual Diner", 
+if st.button("Act as Spencer, the Casual Diner", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
