27ecca4523bbdbe99a4dc9133d1bc4dd4778402c
SpencerTheGuy <spencerwgrant@gmail.com>
2025-08-12 15:47:38 -0400

    casual diner posts added

--- api/backend/Casual_Diner/CasualDiner_routes.py
+++ api/backend/Casual_Diner/CasualDiner_routes.py
@@ -30,3 +30,27 @@ def get_casualdiner():
     the_response = make_response(jsonify(theData))
     the_response.status_code = 200
     return the_response
+    
+
+
+
+# Get all posts associated with CD,
+#followeeid is the ID of the user, seeing all posts made by people they follow
+@casualdiner.route('/CDPost/<followeeid>', methods=['GET'])
+def get_cdposts(followeeid):
+
+    cursor = db.get_db().cursor()
+    the_query = '''SELECT cdp.PostId, cdp.CDId, cdp.Likes, cdp.rating, cdp.share, cdp.bookmark
+FROM CDPost cdp
+         JOIN CasualDiner cd ON cd.CDId = cdp.CDId
+         JOIN Following f ON f.FollowerId = cd.CDId
+WHERE f.followeeID = %s;'''
+    cursor.execute(the_query, (followeeid,))
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+
