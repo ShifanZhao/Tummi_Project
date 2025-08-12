afa27b59b20f2916f1264ad224ec849f8963f974
fabel-e <fabel.e@northeastern.edu>
2025-08-12 17:37:26 -0400

    Explore Changes

--- app/src/pages/02_Explore.py
+++ app/src/pages/02_Explore.py
@@ -6,6 +6,7 @@ import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
 from modules.nav import SideBarLinks
+from numpy.random import default_rng as rng
 
 SideBarLinks()
 
@@ -15,3 +16,35 @@ add_logo("assets/logo.png", height=400)
 # set up the page
 st.markdown("# Explore")
 st.sidebar.header("Explore")
+
+@st.dialog("Recommendations Nearby")
+def show_recommendations_dialog():
+    
+    df = pd.DataFrame({
+    'Name': ["Test"],
+    'lat': [42.361145],
+    'lon': [-71.057083],
+    })
+
+    st.map(df)
+
+    st.write("Restaurants:")
+    st.dataframe(df, hide_index=True)
+    
+    
+@st.dialog("Trending Restaurants")
+def show_trending_dialog():
+    
+    df = pd.DataFrame(
+    rng(0).standard_normal(size=(10, 2)),
+    columns=("Name", "Rating"),
+)
+
+    st.dataframe(df, hide_index=True)
+    
+
+if st.button("Recs Nearby", key="recs_btn"):
+    show_recommendations_dialog()
+
+if st.button("Trending", key="trending_btn"):
+    show_trending_dialog()
--- app/src/pages/11_Profile.py
+++ app/src/pages/11_Profile.py
@@ -69,3 +69,4 @@ st.markdown("""
 
 
 
+
