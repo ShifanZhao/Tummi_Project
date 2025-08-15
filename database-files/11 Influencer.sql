89bff15532dc67cd46bc9f4988cf82377b7d12a6
ShifanZhao <zhao.shif@northeastern.edu>
2025-08-14 21:37:16 -0400

    influencer added

---
+++ database-files/11 Influencer.sql
@@ -0,0 +1,46 @@
+USE Tummi_db;
+INSERT INTO Influencer VALUES (555, 'Fudian', 'efalla0', 'Eben', 'Falla', True, '2024-10-31 00:49:01', False, 5);
+INSERT INTO Influencer VALUES (78, 'Dananshan', 'pwooller1', 'Prisca', 'Wooller', True, '2025-05-02 00:04:30', True, 10);
+INSERT INTO Influencer VALUES (529, 'Sirdaryo', 'apescud2', 'Alysia', 'Pescud', False, '2024-09-13 18:01:04', False, 9);
+INSERT INTO Influencer VALUES (87, 'Byczyna', 'rdonati3', 'Roby', 'Donati', False, '2025-03-17 20:45:03', True, 6);
+INSERT INTO Influencer VALUES (935, 'Edinburgh of the Seven Seas', 'adonlon4', 'Augustine', 'Donlon', False, '2024-09-28 12:04:12', False, 1);
+INSERT INTO Influencer VALUES (513, 'Gaofeng', 'bkembrey5', 'Beale', 'Kembrey', True, '2025-01-21 19:39:28', False, 5);
+INSERT INTO Influencer VALUES (939, 'Talaibon', 'krumin6', 'Kordula', 'Rumin', False, '2024-08-29 00:15:23', False, 5);
+INSERT INTO Influencer VALUES (141, 'Botlhapatlou', 'ctimbrell7', 'Cindelyn', 'Timbrell', True, '2025-08-12 10:40:21', True, 9);
+INSERT INTO Influencer VALUES (480, 'Opinogóra Górna', 'mrizzotto8', 'Marlo', 'Rizzotto', False, '2025-07-20 18:23:16', True, 3);
+INSERT INTO Influencer VALUES (635, 'Xingzi', 'kcradick9', 'Kim', 'Cradick', True, '2024-11-22 19:23:44', False, 7);
+INSERT INTO Influencer VALUES (818, 'Kungsbacka', 'mcattela', 'Melosa', 'Cattel', True, '2025-08-08 20:14:40', False, 10);
+INSERT INTO Influencer VALUES (315, 'Komorniki', 'fdaulbyb', 'Frans', 'DAulby', True, '2025-04-04 11:18:02', False, 2);
+INSERT INTO Influencer VALUES (877, 'Manyava', 'rmcgiveenc', 'Rolf', 'McGiveen', True, '2024-10-08 12:01:02', True, 1);
+INSERT INTO Influencer VALUES (54, 'Zhoutou', 'dwolseleyd', 'Donny', 'Wolseley', False, '2025-03-24 04:32:18', False, 1);
+INSERT INTO Influencer VALUES (960, 'Dłutów', 'fcalderheade', 'Ferrel', 'Calderhead', True, '2025-03-30 01:58:04', True, 6);
+INSERT INTO Influencer VALUES (528, 'Lemery', 'mclericoatesf', 'Milt', 'Clericoates', True, '2024-10-18 14:31:01', True, 10);
+INSERT INTO Influencer VALUES (803, 'Neresnytsya', 'lormang', 'Leann', 'Orman', False, '2024-10-01 06:48:43', False, 7);
+INSERT INTO Influencer VALUES (19, 'Yakovlevskoye', 'kseawellh', 'Kris', 'Seawell', True, '2025-01-29 02:57:06', True, 9);
+INSERT INTO Influencer VALUES (134, 'Torsås', 'laspdeni', 'Luce', 'Aspden', False, '2024-12-04 16:36:44', True, 2);
+INSERT INTO Influencer VALUES (880, 'Kingston', 'mlangsburyj', 'Miguela', 'Langsbury', False, '2025-02-15 08:54:38', True, 6);
+INSERT INTO Influencer VALUES (156, 'Ijuí', 'rmorlingk', 'Rufus', 'Morling', True, '2025-05-24 21:17:38', False, 5);
+INSERT INTO Influencer VALUES (832, 'Xipingle', 'eknelll', 'Eduard', 'Knell', True, '2025-07-19 06:56:11', False, 9);
+INSERT INTO Influencer VALUES (908, 'Daliu', 'abaversorm', 'Arluene', 'Baversor', True, '2025-05-03 05:01:06', True, 3);
+INSERT INTO Influencer VALUES (945, 'Faaone', 'adeardsn', 'Angele', 'Deards', False, '2025-02-26 08:47:28', True, 8);
+INSERT INTO Influencer VALUES (637, 'Toride', 'bionnisiano', 'Bryn', 'Ionnisian', True, '2024-10-22 03:19:37', True, 2);
+INSERT INTO Influencer VALUES (707, 'Chengxiang', 'mebornp', 'Melvyn', 'Eborn', False, '2024-11-04 01:59:16', False, 7);
+INSERT INTO Influencer VALUES (397, 'Bełchatów', 'scristoferiq', 'Sunny', 'Cristoferi', True, '2024-10-17 09:36:50', True, 4);
+INSERT INTO Influencer VALUES (863, 'Chengzi', 'jnewickr', 'Johnathon', 'Newick', True, '2025-05-06 20:09:59', False, 9);
+INSERT INTO Influencer VALUES (596, 'Baizhu', 'hswaffers', 'Hussein', 'Swaffer', True, '2025-04-14 20:45:16', True, 6);
+INSERT INTO Influencer VALUES (574, 'Kang', 'dravelust', 'Deloris', 'Ravelus', True, '2024-12-06 20:27:29', True, 10);
+INSERT INTO Influencer VALUES (739, 'Chujiang', 'eboichatu', 'Elene', 'Boichat', True, '2025-05-18 00:43:56', True, 2);
+INSERT INTO Influencer VALUES (747, 'Flandes', 'sbastenv', 'Samson', 'Basten', False, '2025-06-04 13:41:44', True, 9);
+INSERT INTO Influencer VALUES (828, 'Nyapar', 'cvereyw', 'Celina', 'Verey', False, '2024-12-13 02:45:09', True, 5);
+INSERT INTO Influencer VALUES (982, 'Banjar Penyalin', 'hguitonneaux', 'Haily', 'Guitonneau', False, '2025-04-14 10:38:16', True, 7);
+INSERT INTO Influencer VALUES (322, 'Oslo', 'mkelseyy', 'Marius', 'Kelsey', True, '2025-07-01 04:23:22', True, 6);
+INSERT INTO Influencer VALUES (189, 'Baitu', 'fmcclochz', 'Fanchette', 'McCloch', True, '2025-03-19 18:51:06', True, 9);
+INSERT INTO Influencer VALUES (125, 'Sakurai', 'nplenderleith10', 'Nerta', 'Plenderleith', False, '2025-04-21 01:39:58', True, 1);
+INSERT INTO Influencer VALUES (432, 'Vetlanda', 'ttwentyman11', 'Taddeo', 'Twentyman', False, '2024-12-22 17:35:37', True, 4);
+INSERT INTO Influencer VALUES (465, 'Peukanbada', 'ppail12', 'Polly', 'Pail', True, '2025-02-27 23:19:13', False, 1);
+INSERT INTO Influencer VALUES (630, 'Chengtian', 'agiraudot13', 'Aurilia', 'Giraudot', False, '2025-01-03 06:32:49', True, 1);
+INSERT INTO Influencer
+VALUES (2, 'LA', 'FoodManiac', 'Tiffany', 'Nguyen', TRUE,
+       '2025-08-05 7:51:00.000', FALSE, 1),
+      (8, 'New York City', 'lilgod', 'Lil', 'God', TRUE,
+       '2025-06-10 8:51:00.000', FALSE, 2);
