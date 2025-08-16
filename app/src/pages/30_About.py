cf528f43b941d52d7ec0e5b115ff80e56b5c03e7
ShifanZhao <zhao.shif@northeastern.edu>
2025-08-15 20:41:57 -0400

    ???

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,8 +8,16 @@ st.write("# About this App")
 
 st.markdown(
     """
+<<<<<<< HEAD
     This is a project for CS3200 Introduction to Databases. We’re building Tummi, a data-driven food discovery app that helps users track their restaurant visits, rate dishes, and receive recommendations. It also allows users to add friends to see which restaurants their friends have visited and recommend. This app is important because it enhances users’ experience with finding new restaurants and foods. Traditional food review platforms often are cluttered, impersonal, and outdated. Users are typically overwhelmed by hundreds/thousands of anonymous reviews and generic star ratings. Tummi solves these pain points.
+=======
+    This is a demo app for the Introduction to Databases and Design Course Project.
+>>>>>>> parent of 5b67d25 (Updated About page.)
 
+    The goal of this demo is to display collected information as well as 
+    demonstrate some of the features of the various users. 
+
+    Stay tuned for more information and features to come!
     """
 )
 
